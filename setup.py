ea58e70c9ddc8932df22c6e1b729072e3e9ffceb
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2023-03-23 22:30:23 +0100

    setup: fix link typo
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(name='saih-hidrosur',
-                 version='0.2.0',
+                 version='0.2.1',
                  description='Library around SAIH Hidrosur data',
                  long_description=open('README.md').read().strip(),
                  long_description_content_type="text/markdown",
                  author='Frank Villaro-Dixon',
                  author_email='frank@villaro-dixon.eu',
-                 url='https://www.github.com/Frankkkkk/python-saih-hydrosur',
+                 url='https://www.github.com/Frankkkkk/python-saih-hidrosur',
                  packages=['saihhidrosur'],
                  install_requires=['requests'],
                  license='MIT License',
